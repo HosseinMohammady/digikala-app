5e3f4d5f2d4caa24e6116d31b99a9ca70cdf3898
Hossein Mohammmady <mhossein138585@gmail.com>
2024-04-30 06:55:05 -0700

    feat : add another route for ProductDetailPage

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "products/:id", element: <ProductDetailPage /> },
+    ],
   },
 ]);
 

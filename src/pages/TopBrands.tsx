92233d0c193426527222ced53a9353f0b3dbcb53
Hossein Mohammmady <mhossein138585@gmail.com>
2024-05-01 08:50:12 -0700

    feat(TopBrands.tsx): make an array of top brand images and wrap them in a swiperslide and also making the component responsive

--- src/pages/TopBrands.tsx
+++ src/pages/TopBrands.tsx
@@ -1,4 +1,9 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { isDesktop, isMobile, isTablet } from "react-device-detect";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 
 const images = [
   "../public/brand1.png",
@@ -25,7 +30,47 @@ const images = [
 ];
 
 const TopBrands = () => {
-  return <Container></Container>;
+  return (
+    <Container
+      sx={{
+        border: { lg: "1px solid lightgray" },
+        borderRadius: 5,
+        mt: 3,
+        py: 3,
+      }}
+    >
+      <Stack
+        width="100%"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        gap={1}
+        mb={3}
+      >
+        <Typography variant="h5">محبوب‌ ترین برندها</Typography>
+        <StarBorderOutlinedIcon sx={{ fontSize: 24, color: "warning.main" }} />
+      </Stack>
+      <Swiper
+        slidesPerView={isDesktop ? 10 : isTablet ? 8 : isMobile ? 3.5 : 5}
+        dir="rtl"
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <Box
+              component="img"
+              src={image}
+              sx={{
+                cursor: "pointer",
+                width: { xs: "60px", sm: "75px", lg: "100px" },
+                height: "100px",
+                objectFit: "contain",
+              }}
+            ></Box>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Container>
+  );
 };
 
 export default TopBrands;
